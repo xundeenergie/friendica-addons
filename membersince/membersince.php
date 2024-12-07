82ed43f5076f8d14f7562ab68daf97b8ce8a0afa
Art4 <art4@wlabs.de>
2024-12-07 22:08:59 +0000

    Fix errors

--- membersince/membersince.php
+++ membersince/membersince.php
@@ -7,7 +7,6 @@
  * Status: Unsupported
  */
 
-use Friendica\App;
 use Friendica\Core\Hook;
 use Friendica\DI;
 use Friendica\Util\DateTimeFormat;
@@ -19,7 +18,7 @@ function membersince_install()
 
 function membersince_display(array &$b)
 {
-	if (DI::app()->getCurrentTheme() == 'frio') {
+	if (DI::appHelper()->getCurrentTheme() == 'frio') {
 		// Works in Frio.
 		$doc = new DOMDocument();
 		$doc->loadHTML(mb_convert_encoding($b, 'HTML-ENTITIES', 'UTF-8'));
@@ -39,7 +38,7 @@ function membersince_display(array &$b)
 		$label->setAttribute('class', 'col-lg-4 col-md-4 col-sm-4 col-xs-12 profile-label-name text-muted');
 
 		// The div for the register date of the profile owner.
-		$entry = $doc->createElement('div', DateTimeFormat::local(DI::app()->profile['register_date']));
+		$entry = $doc->createElement('div', DateTimeFormat::local(DI::appHelper()->profile['register_date']));
 		$entry->setAttribute('class', 'col-lg-8 col-md-8 col-sm-8 col-xs-12 profile-entry');
 
 		$div->appendChild($hr);
@@ -50,6 +49,6 @@ function membersince_display(array &$b)
 		$b = $doc->saveHTML();
 	} else {
 		// Works in Vier.
-		$b = preg_replace('/<\/dl>/', "</dl>\n\n\n<dl id=\"aprofile-membersince\" class=\"aprofile\">\n<dt>" . DI::l10n()->t('Member since:') . "</dt>\n<dd>" . DateTimeFormat::local(DI::app()->profile['register_date']) . "</dd>\n</dl>", $b, 1);
+		$b = preg_replace('/<\/dl>/', "</dl>\n\n\n<dl id=\"aprofile-membersince\" class=\"aprofile\">\n<dt>" . DI::l10n()->t('Member since:') . "</dt>\n<dd>" . DateTimeFormat::local(DI::appHelper()->profile['register_date']) . "</dd>\n</dl>", $b, 1);
 	}
 }
